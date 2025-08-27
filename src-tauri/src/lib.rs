a732bcfba33c2dd3d760299a7642177e58cff4d4
kanatapple <edamame1224@gmail.com>
2025-08-27 15:39:01 +0900

    first commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,21 @@
+use std::time::Duration;
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+async fn greet(name: &str) -> Result<String, String> {
+    println!("greet invoked");
+
+    let duration = Duration::from_millis(10000);
+    tokio::time::sleep(duration).await;
+
+    Ok(format!("Hello, {}! You've been greeted from Rust!", name))
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
